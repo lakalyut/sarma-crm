1760d9b5751b79ff949f79253784c95698ce9a7a
Alex <alexkopylov322@gmail.com>
2026-02-13 14:41:00 +0800

    COMMIT 2: DEPLOY

--- app/database.py
+++ app/database.py
@@ -1,10 +1,17 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./main.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./main.db")
+
+# connect_args нужны только для SQLite
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args, future=True)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
 Base = declarative_base()
 
 def get_db():
