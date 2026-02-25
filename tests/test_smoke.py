7e1c8e5a15b12ad3999c15da7ec4af03e9e95833
Alex <alexkopylov322@gmail.com>
2026-02-25 15:02:25 +0800

    C.8: WF

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -6,11 +6,12 @@ client = TestClient(app)
 
 
 def test_root_redirect():
-    r = client.get("/", allow_redirects=False)
-    assert r.status_code in (302, 307)
+    r = client.get("/", follow_redirects=False)
+    assert r.status_code in (301, 302, 307, 308)
+    assert r.headers["location"].startswith("/admin/products")
 
 
-def test_login_page_ok():
-    r = client.get("/login")
-    # если логина нет — убери этот тест, но лучше сразу добавить login
-    assert r.status_code in (200, 404)
+#def test_login_page_ok():
+#    r = client.get("/login")
+#    # если логина нет — убери этот тест, но лучше сразу добавить login
+#    assert r.status_code in (200, 404)
